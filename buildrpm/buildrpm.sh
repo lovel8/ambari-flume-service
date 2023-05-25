99a2bc1efb7e25b1f4003c2b98e1067a04c543ef
LoveL <77282562+lovel8@users.noreply.github.com>
2023-05-25 12:13:47 +0800

    Update buildrpm.sh

--- buildrpm/buildrpm.sh
+++ buildrpm/buildrpm.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-flume_url="https://downloads.apache.org/flume/1.9.0/apache-flume-1.9.0-bin.tar.gz"
+# flume_url="https://downloads.apache.org/flume/1.9.0/apache-flume-1.9.0-bin.tar.gz"
+flume_url="https://archive.apache.org/dist/flume/1.9.0/apache-flume-1.9.0-bin.tar.gz"
 flume_tar='apache-flume-1.9.0-bin.tar.gz'
 tar_path="rpmbuild/SOURCES/$flume_tar"
 mkdir -pv rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES}
